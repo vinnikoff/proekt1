615faebe7d6b94a61db6c73d84b49f3e9e1ca355
vinnikoff <winr7post@gmail.com>
2016-10-30 20:40:54 +0200

    vinnikov

---
+++ src/vin.java
@@ -0,0 +1,14 @@
+/**
+ * Created by VINNI on 30.10.16.
+ */
+public class vin {
+
+    public static void main (String[] args)
+    {
+        System.out.println("Мой первый класс на Java");
+
+
+
+    }
+
+}
